3b772c3fbc0413e90a6246bc48fb922a9329a3fa
zhs <alezy@ya.ru>
2016-03-21 21:30:42 +0300

    bul ref 1

--- buldozer.rb
+++ buldozer.rb
@@ -9,15 +9,15 @@ class Buldozer
   end
 
   def right
-    look(1,0)	
+    look(1, 0)
   end
 
   def up
-    look(0,-1)
+    look(0, -1)
   end
 
   def down
-    look(0,1)
+    look(0, 1)
   end
 
   private
@@ -26,34 +26,37 @@ class Buldozer
     current_row = @map.buldozer_position[0]
     current_column = @map.buldozer_position[1]
 
-    case @map.map[current_row+updown][current_column+leftright]
+    case @map.map[current_row + updown][current_column + leftright]
     # wall.
-    when 4,5
+    when 4, 5
       nil
     # empty or trap
     when 0
       @map.map[current_row][current_column] = 0
-      @map.map[current_row + updown][current_column + leftright] = 1	
+      @map.map[current_row + updown][current_column + leftright] = 1
     # stone
     when 2
-      case @map.map[current_row + updown * 2][current_column + leftright * 2]
-      when 4,2,5
-        nil
-      when 0
-        move_stone(leftright, updown, current_row, current_column)
-      # stone trap
-      when 3
-        move_stone(leftright, updown, current_row, current_column)
-        # trapped!
-        @map.map[current_row + updown * 2][current_column + leftright * 2] = 5
-      end
+      look_stone(leftright, updown, current_row, current_column)
     end
-		
   end
 
   def move_stone(leftright, updown, current_row, current_column)
     @map.map[current_row][current_column] = 0
     @map.map[current_row + updown][current_column + leftright] = 1
     @map.map[current_row + updown * 2][current_column + leftright * 2] = 2
+  end
+
+  def look_stone(leftright, updown, current_row, current_column)
+    case @map.map[current_row + updown * 2][current_column + leftright * 2]
+    when 4, 2, 5
+      nil
+    when 0
+      move_stone(leftright, updown, current_row, current_column)
+    # stone trap
+    when 3
+      move_stone(leftright, updown, current_row, current_column)
+      # trapped to 5!
+      @map.map[current_row + updown * 2][current_column + leftright * 2] = 5
+    end
   end
 end
